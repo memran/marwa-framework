9d87ede14a39208cde983528eebdac44be6fae76
Mohammad Emran <memran.dhk@gmail.com>
2023-04-27 21:51:59 +0600

    key:generate command added

---
+++ src/Application/Commands/BasicCommands/KeyGenerate.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * @author    Mohammad Emran <memran.dhk@gmail.com>
+ * @copyright 2018
+ *
+ * @see https://www.github.com/memran
+ * @see http://www.memran.me
+ */
+
+namespace Marwa\Application\Commands\BasicCommands;
+
+use Marwa\Application\Commands\AbstractCommand;
+
+class KeyGenerate extends AbstractCommand
+{
+    /**
+     * [$name description]
+     *
+     * @var string
+     */
+    var $name = "key:generate";
+    /**
+     * [$description description]
+     *
+     * @var string
+     */
+    var $description = "Application Key Generate";
+    /**
+     * [$help description]
+     *
+     * @var string
+     */
+    var $help = "key:generate will help you to set Application Key.";
+
+
+    /**
+     * [handle description] this will execute and print sql results on table
+     *
+     * @return [type] [description]
+     */
+    public function handle(): void
+    {
+        $keyStr = Str::random(32);
+        $this->info("Application Key is " + $keyStr);
+    }
+
+
+
+
+
+}
--- src/Application/Utils/Str.php
+++ src/Application/Utils/Str.php
@@ -1,28 +1,39 @@
 <?php
-	namespace Marwa\Application\Utils;
-	
-	use Nette\Utils\Strings as NStrings;
-	
-	class Str extends NStrings {
-		
-		/**
-		 * @param string $search
-		 * @param array $searchTerm
-		 * @return bool
-		 */
-		public static function containsArray( string $search, array $searchTerm )
-		{
-			$result = false;
-			foreach ( $searchTerm as $k )
-			{
-				$result = self::contains($search, self::upper($k));
-				if ( $result === true )
-				{
-					return $result;
-				}
+namespace Marwa\Application\Utils;
+
+use Nette\Utils\Strings as NStrings;
+
+class Str extends NStrings
+{
+
+	/**
+	 * @param string $search
+	 * @param array $searchTerm
+	 * @return bool
+	 */
+	public static function containsArray(string $search, array $searchTerm)
+	{
+		$result = false;
+		foreach ($searchTerm as $k) {
+			$result = self::contains($search, self::upper($k));
+			if ($result === true) {
+				return $result;
 			}
-			
-			return $result;
 		}
-		
+
+		return $result;
 	}
+
+	public static function random(int $n = 16): string
+	{
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$randomString = '';
+
+		for ($i = 0; $i < $n; $i++) {
+			$index = rand(0, strlen($characters) - 1);
+			$randomString .= $characters[$index];
+		}
+
+		return $randomString;
+	}
+}
