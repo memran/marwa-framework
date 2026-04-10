6f9afa625a30c0d47879fca4f8cd013e71b5ebe6
Mohammad Emran <memran.dhk@gmail.com>
2026-04-10 18:46:15 +0600

    style: fix import order and unused imports
    - Order imports alphabetically in DbDropCommand and DbCreateCommand
    - Remove unused InputArgument import in DbBackupCommand
    - Add trailing newline to DBForge

--- src/Console/Commands/DbBackupCommand.php
+++ src/Console/Commands/DbBackupCommand.php
@@ -9,7 +9,6 @@ use Marwa\Framework\Database\DBForge;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
