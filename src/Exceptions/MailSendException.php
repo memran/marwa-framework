7c36414490622575991529990517b368347b8839
Mohammad Emran <memran.dhk@gmail.com>
2026-04-23 12:37:31 +0600

    Fix code style: use single-line empty body for MailSendException

--- src/Exceptions/MailSendException.php
+++ src/Exceptions/MailSendException.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Marwa\Framework\Exceptions;
 
-final class MailSendException extends \RuntimeException
-{
-}
+final class MailSendException extends \RuntimeException {}
